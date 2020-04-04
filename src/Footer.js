d2fef5b770456722c89528498291b5ea855da90c
shashankd48 <jfriday464@gmail.com>
2020-04-04 19:10:08 +0530

    Basic Template Done

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+    <section className="footer bg-dark p-5 text-center text-white">
+      <p className="lead container">Contact us at Instagram</p>
+      <p className="lead container">You can react via email too</p>
+      <p className="lead container">NO COPYRIGHT</p>
+    </section>
+)
+export default Footer
