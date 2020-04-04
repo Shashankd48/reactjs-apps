8a8215fbdffb405697867d48b57465e5252f3167
shashankd48 <jfriday464@gmail.com>
2020-04-04 19:38:58 +0530

    CardSection optimized As Card

--- src/CardSection.js
+++ src/CardSection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Card from './Card'
 const CardSection = () => {
     return (
         <section className="contact bg-success ">
@@ -7,57 +7,27 @@ const CardSection = () => {
                 <h2 className="text-white text-center">
                     We love new friends!
                 </h2>
-                <div className="row pb-5 d-flex justify-content-center">
+                <div className="row pb-5">
                     <div className="col-lg-4 col-sm-12 col-md-6 mb-5">
-                        <div className="card" style={{width: "18rem"}}>
-                            <img
-                                src="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                                className="card-img-top"
-                                alt="..."
-                            />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                                </p>
-                                <a href="#" className="btn btn-success">Go somewhere</a>
-                            </div>
-                        </div>
+                        <Card 
+                            title= "Shashank Dubey" 
+                            buttonText="About Me"
+                            imageSrc = "https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                        />
                     </div>
                     <div className="col-lg-4 col-sm-12 col-md-6 mb-5">
-                        <div className="card" style={{width: "18rem"}}>
-                            <img
-                                src="https://images.pexels.com/photos/3532544/pexels-photo-3532544.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                                className="card-img-top"
-                                alt="..."
-                            />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                                </p>
-                                <a href="#" className="btn btn-success">Go somewhere</a>
-                            </div>
-                        </div>
+                        <Card
+                            title = "Dhananjay Pathak"
+                            buttonText = "Know Me"
+                            imageSrc = "https://images.pexels.com/photos/3532544/pexels-photo-3532544.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                        />
                     </div>
                     <div className="col-lg-4 col-sm-12 col-md-6 mb-5">
-                        <div class="card" style={{width: "18rem"}}>
-                            <img
-                                src="https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                                class="card-img-top"
-                                alt="..."
-                            />
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                                </p>
-                                <a href="#" class="btn btn-success">Go somewhere</a>
-                            </div>
-                        </div>
+                        <Card
+                            title = "Vikas Pandey"
+                            buttonText = "Say Hi"
+                            imageSrc = "https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"                        
+                         />
                     </div>
                 </div>
             </div>
