230fd49f607159c2de3e8e6e68ab5bf906f4e924
shashankd48 <jfriday464@gmail.com>
2020-04-05 10:56:09 +0530

    Basic Counter App completed in reactjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import './App.css'
 const App = () => {
+
+    const [count, setCount] = useState(0);
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className="App">
+            <header>
+                <h1>Counter App using State/hooks</h1>
+            </header>
+            <h2>Current value of counter is : {count}</h2>
+
+            <button onClick={() => setCount(0)}>
+                Reset Counter
+            </button>
+            <button onClick={() => count>10 ? "" : setCount(count + 1)}>
+                Increase Counter
+            </button>
+            <button onClick={() => count<=0 ? "" : setCount(count - 1)}>
+                Decrease Counter
+            </button>
         </div>
     )
 }
 
-export default App
+export default App;
