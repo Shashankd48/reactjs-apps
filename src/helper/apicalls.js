5e11253dd14308f50318c9754184e26f3cd7e3a3
shashankd48 <jfriday464@gmail.com>
2020-05-01 21:13:34 +0530

    Navigation bar fixed

--- src/helper/apicalls.js
+++ src/helper/apicalls.js
@@ -1,8 +1,13 @@
+require("dotenv").config({
+   path: `.env.${process.env.APIKEY}`,
+});
+const api = process.env.APIKEY;
+// Authorization: "ba382022-509d3c06-6f1d7aa9-44dc55b6",
 export const upCommingItems = () => {
    return fetch("https://fortniteapi.io/items/upcoming?lang=en", {
       method: "GET",
       headers: {
-         Authorization: "ba382022-509d3c06-6f1d7aa9-44dc55b6",
+         Authorization: `${api}`,
       },
    })
       .then((response) => {
